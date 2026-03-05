6b9e94c58dde293596f0505fe803f3f2e54f645b
Aunnno <2307738723@qq.com>
2026-03-05 13:13:49 +0800

    add:qrcode detect

--- backend/src/router/__init__.py
+++ backend/src/router/__init__.py
@@ -4,6 +4,7 @@ from src.router.voice import voice_router
 from src.router.mapping import map_router
 from src.router.medical_system import medical_system_router
 from src.router.user import user_router
+from src.router.qrcode import qrcode_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(triager_router)
@@ -11,3 +12,4 @@ api_router.include_router(voice_router)
 api_router.include_router(map_router)
 api_router.include_router(medical_system_router)
 api_router.include_router(user_router)
+api_router.include_router(qrcode_router)
